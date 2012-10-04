5fa2216920371ece8a9643c6a08608d9f0c3f3e1
Michael Spall <spalmich@isu.edu>
2012-10-04 14:19:00 -0600

    Avoid DB error setting user.address to NULL.
    If $lmbperson->adrstreet is not set, do not set $moodleuser->address
    to NULL when updating user. If $moodleuser->address is NULL, an error
    will occur trying to set user.address to NULL. Also changed behavior
    to match $moodleuser->phone1. Instead of $moodleuser->address = '',
    do not set $moodleuser->address if $lmbperson->adrstreet is not set
    or when ($config->includeaddress && $config->forceaddress) is false.

--- lib.php
+++ lib.php
@@ -1917,7 +1917,9 @@ class enrol_lmb_plugin extends enrol_plugin {
                     }
 
                     if ($this->get_config('includeaddress') && $this->get_config('forceaddress')) {
-                        $moodleuser->address = $lmbperson->adrstreet;
+                        if (isset($lmbperson->adrstreet)) {
+                            $moodleuser->address = $lmbperson->adrstreet;
+                        }
 
                         if ($this->get_config('defaultcity') == 'standardxml') {
                             if ($lmbperson->locality) {
@@ -1930,8 +1932,6 @@ class enrol_lmb_plugin extends enrol_plugin {
                         } else if ($this->get_config('defaultcity') == 'standard') {
                             $moodleuser->city = $this->get_config('standardcity');
                         }
-                    } else {
-                        $moodleuser->address = '';
                     }
 
                     if (enrol_lmb_compare_objects($moodleuser, $oldmoodleuser) || ($this->get_config('customfield1mapping')
@@ -1987,8 +1987,6 @@ class enrol_lmb_plugin extends enrol_plugin {
                     if ($this->get_config('includeaddress')) {
                         if (isset ($lmbperson->adrstreet)) {
                             $moodleuser->address = $lmbperson->adrstreet;
-                        } else {
-                            $moodleuser->address = '';
                         }
 
                         if ($this->get_config('defaultcity') == 'standardxml') {
@@ -2003,8 +2001,6 @@ class enrol_lmb_plugin extends enrol_plugin {
                             $moodleuser->city = $this->get_config('standardcity');
                         }
 
-                    } else {
-                        $moodleuser->address = '';
                     }
 
                     $moodleuser->country = $CFG->country;
