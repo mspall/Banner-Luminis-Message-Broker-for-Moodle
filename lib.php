4b974c39bf7814853a59c656d66113c1f4e5e2ee
Michael Spall <spalmich@isu.edu>
2016-02-12 09:47:01 -0700

    ISU Use username@isu.edu if email missing in xml
    (ISU specific) If <email> missing in xml, use username@isu.edu.
    This avoids skipping users that don't have an email address in xml.

--- lib.php
+++ lib.php
@@ -1828,6 +1828,10 @@ class enrol_lmb_plugin extends enrol_plugin {
         }
         if (isset($person->email)) {
             $lmbperson->email = $person->email;
+        } else {
+            if (isset($person->username)) {
+                $lmbperson->email = "$person->username" . '@isu.edu';
+            }
         }
         if (isset($person->username)) {
             $lmbperson->username = $person->username;
